8a7e4fe41a36b34daacdb8a90f879912862d6197
Mayu Hayakawa <mayu.i.19930327@gmail>
2022-09-15 20:43:05 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import {Component} from 'react';
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  } 
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => this.setState(() => {
+          return { monsters: users }
+        }
+      ))
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase()
+    this.setState(
+      () => {
+      return { searchField };
+      }
+    )
+  }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filterMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    })
+
+    return (
+      <div className='App'>
+        <SearchBox
+          className='monsters-search-box'
+          onChangeHandler={onSearchChange}
+          placeholder='search monsters' />
+        <CardList monsters={filterMonsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
